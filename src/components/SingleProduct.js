12b8cc829ee628f81ffc89ff9f64eb89a0ee7965
shadofficial <azmi.shad@gmail.com>
2022-08-08 00:45:15 +0530

    Ui issue fixed

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -38,17 +38,6 @@ const SingleProduct = ({ prod }) => {
 
   return (
     <Container fluid="md">
-
-<div class="row g-0 bg-light position-relative">
-  <div class="col-md-6 mb-md-0 p-md-4">
-    <img src="https://thumbs.dreamstime.com/b/aloo-paratha-indian-potato-stuffed-flatbread-butter-top-served-fresh-sweet-lassi-chutney-pickle-selective-focus-lassie-164213775.jpg"  alt="..."/>
-  </div>
-  <div class="col-md-6 p-4 ps-md-0">
-    <h5 class="mt-0">Columns with stretched link</h5>
-    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
-    <a href="#" class="stretched-link">Go somewhere</a>
-  </div>
-</div>
       <Row className="mb-5">
         <Col md={4} xs={4}>
           <Image
