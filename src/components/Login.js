97080901de54b0841d847b1f1ca43fd980c986ea
shadofficial <azmi.shad@gmail.com>
2022-08-07 22:59:27 +0530

    login form changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,74 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
- 
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Login(props) {
-  const username = useFormInput('');
-  const password = useFormInput('');
+  const username = useFormInput("");
+  const password = useFormInput("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
- 
+
   // handle button click of login form
   const handleLogin = () => {
-    props.history.push('/Order');
-  }
- 
+    props.history.push("/Order");
+  };
+
   return (
-    <div>
-<Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Login</Form.Label>
-        <Form.Control type="text" {...username}  placeholder="Enter your name" />
-        <Form.Text className="text-muted">
-          We'll never share your details with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container fluid="md">
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Login</Form.Label>
+              <Form.Control
+                type="text"
+                {...username}
+                placeholder="Enter your name"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your details with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Number</Form.Label>
-        <Form.Control type="number" {...password} placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit"  onClick={handleLogin} disabled={loading}>
-        Submit
-      </Button>
-    </Form>
-
-    </div>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Number</Form.Label>
+              <Form.Control
+                type="number"
+                {...password}
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleLogin}
+              disabled={loading}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
- 
-const useFormInput = initialValue => {
+
+const useFormInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
- 
-  const handleChange = e => {
+
+  const handleChange = (e) => {
     setValue(e.target.value);
-  }
+  };
   return {
     value,
-    onChange: handleChange
-  }
-}
- 
+    onChange: handleChange,
+  };
+};
+
 export default Login;
