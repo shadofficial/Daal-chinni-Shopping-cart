c1635a71f460cf3c6b1d082640ee050dea2d1f7b
Shad <53861418+shadofficial@users.noreply.github.com>
2022-11-08 18:44:45 +0530

    Update Reducers.js

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -36,13 +36,4 @@ export const cartReducer = (state, action) => {
   }
 };
 
-export const productReducer = (state, action) => {
-  switch (action.type) {
-    case "FILTER_BY_SEARCH":
-      return { ...state, searchQuery: action.payload };
-    case "CLEAR_FILTERS":
-      return { byStock: false, byFastDelivery: false, byRating: 0 };
-    default:
-      return state;
-  }
-};
+
